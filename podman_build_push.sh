05888eac1a51050abb08ccafca809013656d8543
jstakun <jstakun@redhat.com>
2022-11-12 11:57:25 +0100

    throttling

--- podman_build_push.sh
+++ podman_build_push.sh
@@ -1,3 +1,5 @@
-podman login quay.io
-podman build -f Containerfile.distroless . -t quay.io/jstakun/json-proxy:latest
-podman push quay.io/jstakun/json-proxy:latest
+VERSION=latest
+AUTHFILE=~/jsonproxy.auth
+#podman login quay.io
+podman build --squash -f Containerfile.distroless . -t quay.io/jstakun/json-proxy:$VERSION
+podman push --authfile $AUTHFILE quay.io/jstakun/json-proxy:$VERSION
