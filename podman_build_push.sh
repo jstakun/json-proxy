4bd774f263bd5008f1012a6edbfb027488e85ae9
jstakun <jstakun@redhat.com>
2022-11-23 13:53:45 +0100

    Containerfile

--- podman_build_push.sh
+++ podman_build_push.sh
@@ -1,5 +1,5 @@
 VERSION=latest
 AUTHFILE=~/jsonproxy.auth
 #podman login quay.io
-podman build --squash -f Containerfile.distroless . -t quay.io/jstakun/json-proxy:$VERSION
+podman build --squash -f Containerfile.quarkus . -t quay.io/jstakun/json-proxy:$VERSION
 podman push --authfile $AUTHFILE quay.io/jstakun/json-proxy:$VERSION
